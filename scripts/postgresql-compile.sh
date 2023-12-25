9cd21e7607e9bf77b9b14754186a1bba9a2f0e31
rahnoff <frombayern@gmail.com>
2023-12-25 23:26:58 +0300

    Add Patroni role

--- scripts/postgresql-compile.sh
+++ scripts/postgresql-compile.sh
@@ -1,42 +1,54 @@
 if [ "$#" -lt 2 ]
 then
-	echo "Usage: `basename $0` Debian 12 or `basename $0` RHEL 14"
-	echo -e "Debian or RHEL - Linux flavor, 12 or 14 - PostgreSQL version"
-	exit 1
+  echo "Usage: `basename $0` Debian 12 or `basename $0` RHEL 14"
+  echo "Debian or RHEL - Linux flavor, 12 or 14 - PostgreSQL version"
+  exit 1
 fi
 
-flavor="$1"
-version="$2"
+linux_flavor="$1"
 
-echo -e "Compiling PostgreSQL $version\n"
+postgresql_version="$2"
 
-docker image build --build-arg version="$version" \
-                   -f ../dockerfiles/PostgreSQL-$flavor \
-                   -t postgresql-binaries-all:latest .
+echo -e "Compiling PostgreSQL $postgresql_version\n"
+
+docker image build --build-arg version="$postgresql_version" \
+  -f ../dockerfiles/PostgreSQL-"$linux_flavor" \
+  -t postgresql-initialized:latest .
 
 echo -e "\n"
 
-echo -e "Copying postgresql directory"
+echo -e "Copying /usr/local/postgresql and config files\n"
 
-docker container cp $(docker container create --name postgresql \
-       postgresql-binaries-all:latest):/usr/local/postgresql .
+files_to_extract=("/usr/local/postgresql" "/var/lib/postgresql/pg_hba.conf" \
+  "/var/lib/postgresql/pg_ident.conf" "/var/lib/postgresql/postgresql.conf")
 
-tar -cvf postgresql.tar postgresql
+for item in ${files_to_extract[@]}
+do
+  docker container cp $(docker container create \
+    postgresql-initialized:latest):$item .
+done
 
-gzip postgresql.tar
+tar -cf postgresql.tar postgresql
+
+tar -cf postgresql-conf.tar pg_hba.conf pg_ident.conf postgresql.conf
+
+gzip postgresql.tar postgresql-conf.tar
 
 mv postgresql.tar.gz ../ansible-playbooks/roles/postgresql/files
 
-rm -rf postgresql
+mv postgresql-conf.tar.gz ../ansible-playbooks/roles/postgresql/files
+
+rm -rf postgresql pg_hba.conf pg_ident.conf postgresql.conf
 
 echo -e "\n"
 
-echo -e "Removing a container\n"
+echo -e "Removing containers\n"
 
-docker container rm postgresql
+docker container rm $(docker container ls -a | \
+  grep "postgresql-initialized:latest" | awk '{print $1}')
 
 echo -e "\n"
 
 echo -e "Removing an image\n"
 
-docker image rm postgresql-binaries-all:latest
+docker image rm postgresql-initialized:latest

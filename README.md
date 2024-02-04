9b2b8da17690911fbdf7dce694761ca7a63f60e6
rahnoff <frombayern@gmail.com>
2024-02-04 17:37:28 +0300

    Add several roles

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# PostgreSQL with PgTune and PgBouncer installation
+# PostgreSQL with Patroni and PgBouncer
 
-Intended for an offline setting up, tested on Debian 11. `acl` and<br>
-`psycopg2-binary` should be installed on remote machines. All components,<br>
-except PgTune, are compiled from source within Docker.<br>
+Intended for an offline setting up, tested on Debian 11. All components,<br>
+except are compiled from source within Docker.<br>
 
 ## 1 How to run
 
 On a machine with Internet link and Docker, commence with Bash scripts<br>
 from `scripts` directory in the following order:<br>
 - `bash postgresql-compile.sh Debian 12`<br>
+- `bash patroni-pex.sh`
 - `bash pgbouncer-compile.sh`<br>
+
 That compiles binaries and places them into appropriate folders in<br>
 `ansible-playbooks` one as tarballs. Then a playbook can be started<br>
 with `ansible-playbook -i inventories/dev.yml playbooks/install.yml`.
--- scripts/postgresql-compile.sh
+++ scripts/postgresql-compile.sh
@@ -19,24 +19,27 @@ echo -e "\n"
 
 echo -e "Copying /usr/local/postgresql and config files\n"
 
-files_to_extract=("/usr/local/postgresql" "/var/lib/postgresql/pg_hba.conf" \
-  "/var/lib/postgresql/pg_ident.conf" "/var/lib/postgresql/postgresql.conf")
+# files_to_extract=("/usr/local/postgresql" "/var/lib/postgresql/pg_hba.conf" \
+#   "/var/lib/postgresql/pg_ident.conf" "/var/lib/postgresql/postgresql.conf")
 
-for item in ${files_to_extract[@]}
-do
-  docker container cp $(docker container create \
-    postgresql-initialized:latest):$item .
-done
+# for item in ${files_to_extract[@]}
+# do
+#   docker container cp $(docker container create \
+#     postgresql-initialized:latest):$item .
+# done
+
+docker container cp $(docker container create \
+  postgresql-initialized:latest):/usr/local/postgresql .
 
 tar -cf postgresql.tar postgresql
 
-tar -cf postgresql-conf.tar pg_hba.conf pg_ident.conf postgresql.conf
+# tar -cf postgresql-conf.tar pg_hba.conf pg_ident.conf postgresql.conf
 
-gzip postgresql.tar postgresql-conf.tar
+gzip postgresql.tar # postgresql-conf.tar
 
 mv postgresql.tar.gz ../ansible-playbooks/roles/postgresql/files
 
-mv postgresql-conf.tar.gz ../ansible-playbooks/roles/postgresql/files
+# mv postgresql-conf.tar.gz ../ansible-playbooks/roles/postgresql/files
 
 rm -rf postgresql pg_hba.conf pg_ident.conf postgresql.conf
 

de99c5f47261d58a631b020e4140c8ba60e79ac2
rahnoff <gbennett281@gmail.com>
2024-07-16 14:34:52 -0500

    Edit files

--- compile_components.sh
+++ compile_components.sh
@@ -21,7 +21,7 @@ set -e
 #   0 if tarballs have been created, non-zero on error.
 #############################################################################
 function compile_patroni() {
-  pushd "$(git rev-parse --show-toplevel)" 1> /dev/null
+  pushd "$(git rev-parse --show-toplevel)" 1> /dev/null || exit
 
   local executable
 
@@ -32,17 +32,22 @@ function compile_patroni() {
   readonly executables
 
   docker image build \
-    --build-arg python_version="$2" \
-    -f - \
-    -t patroni:latest .
+    -f dockerfiles/Patroni \
+    -t patroni:latest \
+    --build-arg image_sha256="$2" \
+    --build-arg python_version="$3" \
+    --no-cache \
+    .
 
   for executable in "${executables[@]}"; do
     docker container cp \
-      $(docker container create patroni:latest):/patroni/"${executable}" .
+      $(docker container create patroni:latest):/opt/patroni/"${executable}" .
 
     tar -cf "${executable}".tar "${executable}"
 
     gzip -f "${executable}".tar
+
+    mv "${executable}".tar.gz ansible-playbooks/roles/patroni/files
     
     rm -rf "${executable}"
   done
@@ -61,20 +66,23 @@ function compile_patroni() {
 
 
 function compile_postgresql() {
-  pushd "$(git rev-parse --show-toplevel)" 1> /dev/null
+  pushd "$(git rev-parse --show-toplevel)" 1> /dev/null || exit
 
   local dockerfile
 
   if [[ "$2" == 'debian' ]]; then
     dockerfile='dockerfiles/PostgreSQL-Debian'
+    readonly dockerfile
   elif [[ "$2" == 'rhel' ]]; then
     dockerfile='dockerfiles/PostgreSQL-RHEL'
+    readonly dockerfile
   fi
   
   docker image build \
     -f "$(echo "${dockerfile}")" \
     -t postgresql:latest \
     --build-arg version="$3" \
+    --no-cache \
     .
 
   docker container cp \
@@ -83,6 +91,8 @@ function compile_postgresql() {
   tar -cf postgresql.tar postgresql
 
   gzip -f postgresql.tar
+
+  mv postgresql.tar.gz ansible-playbooks/roles/postgresql/files
 
   rm -rf postgresql
 
@@ -107,7 +117,7 @@ function main() {
   - PostgreSQL\n'
       echo 'By the way of illustration:
   $ compile_components postgresql debian 12.18
-  $ compile_components patroni 3.9.6 sha256:546kl45j654jk6jk546'
+  $ compile_components patroni sha256:736b76eb3f64778646ce0051fb5fed4dfbf67016e51563946230ca8bb40ac687 3.9.6'
       ;;
     patroni)
       if ! compile_patroni "$@"; then
